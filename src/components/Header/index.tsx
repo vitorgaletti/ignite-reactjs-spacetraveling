3c92ada70c1461a76d6766a963745bf842a6a58a
Vitor Galeti <vitor.fgaletti@gmail.com>
2022-02-11 11:47:15 -0300

    :lipstick: Create style Header Component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,12 @@
 import Link from 'next/link';
 import styles from './header.module.scss';
 
-export default function Header() {
+export default function Header(): JSX.Element {
   return (
-    <header className={styles.headerContainer}>
+    <header className={styles.postHeader}>
       <Link href="/">
         <a>
-          <img src="../logo.svg" alt="logo" />
+          <img src="./logo.svg" alt="logo" />
         </a>
       </Link>
     </header>

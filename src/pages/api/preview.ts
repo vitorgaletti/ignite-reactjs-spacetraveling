1d9540838de45128c850e9f073638849f05ed405
Vitor Galeti <vitor.fgaletti@gmail.com>
2022-02-15 16:26:53 -0300

    :construction: Prismic document preview

---
+++ src/pages/api/preview.ts
@@ -0,0 +1,31 @@
+/* eslint-disable consistent-return */
+import { Document } from '@prismicio/client/types/documents';
+import { getPrismicClient } from '../../services/prismic';
+
+function linkResolver(doc: Document): string {
+  if (doc.type === 'posts') {
+    return `/post/${doc.uid}`;
+  }
+  return '/';
+}
+
+const Preview = async (req, res): Promise<void> => {
+  const { token: ref, documentId } = req.query;
+  const redirectUrl = await getPrismicClient(req)
+    .getPreviewResolver(ref, documentId)
+    .resolve(linkResolver, '/');
+
+  if (!redirectUrl) {
+    return res.status(401).json({ message: 'Invalid token' });
+  }
+
+  res.setPreviewData({ ref });
+  res.writeHead(302, { Location: `${redirectUrl}` });
+  res.write(
+    `<!DOCTYPE html><html><head><meta http-equiv="Refresh" content="0; url=${redirectUrl}" />
+    <script>window.location.href = '${redirectUrl}'</script>
+    </head>`
+  );
+  res.end();
+};
+export default Preview;
